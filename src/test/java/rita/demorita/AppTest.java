f9b7c287dc2a8c1780807b65647084c2bb3ebe23
kennedy <ezebuilokennedy@gmail.com>
2023-07-21 17:24:43 +0100

    first commit for demorita ife

---
+++ src/test/java/rita/demorita/AppTest.java
@@ -0,0 +1,12 @@
+package rita.demorita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
